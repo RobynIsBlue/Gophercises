378226a8a63dd12e854b8d3c603f8fd3cde9d930
RobynIsBlue <tyglivermont@gmail.com>
2025-07-24 12:53:03 -0500

    ookko

--- chooseAdv/main.go
+++ chooseAdv/main.go
@@ -27,13 +27,14 @@ type world struct {
 
 func main() {
 	base, _ := template.ParseFiles("base.html")
-	fs := http.FileServer(http.Dir("./static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		base.Execute(w, world{World: "worldy"})
 	})
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/css"))))
 	fmt.Println("serving on port http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	storyArc := r.PathValue("story_arc")
+	http.ServeFile(w, r, fmt.Sprintf("%s.html", storyArc))
+}
